0e489c26e303fe7089cad5c1db75916868c4cce2
Max Helmetag <mhelmetag@gmail.com>
2020-06-30 00:34:21 -0700

    use utc everywhere

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://localhost/stoke_archives') 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv(
+    'DATABASE_URL', 'postgresql://localhost/stoke_archives')
+engine = create_engine(DATABASE_URL, connect_args={
+                       "options": "-c timezone=utc"})
 
 Session = sessionmaker(bind=engine)

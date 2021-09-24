313cba35fe6e431be7e0e08f8b0c67272772cd4b
Max Helmetag <mhelmetag@gmail.com>
2021-09-23 21:30:33 -0700

    really fix heroku postgres

--- app/db/session.py
+++ app/db/session.py
@@ -9,7 +9,7 @@ DATABASE_URL = os.getenv(
 
 # heroku using postgres:// but sqlalchemy wanting postgresql://
 CORRECTED_DATABASE_URL = re.sub('postgres:', 'postgresql:', DATABASE_URL)
-engine = create_engine(DATABASE_URL, connect_args={
+engine = create_engine(CORRECTED_DATABASE_URL, connect_args={
                        'options': '-c timezone=utc'})
 
 Session = sessionmaker(bind=engine)
